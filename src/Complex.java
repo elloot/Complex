a574da5a996334db60a24a1eac3b916001e0e1c2
elloot <elloot@github.com>
2020-01-31 11:07:51 +0100

    add constructor

---
+++ src/Complex.java
@@ -0,0 +1,16 @@
+/**
+ * A program that handles complex numbers
+ * 2020-01-31
+ * Author: Elliot Duchek
+ */
+public class Complex {
+    private double a;
+    private double b;
+
+    public Complex(double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
+
+
+}
