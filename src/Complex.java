8ff122ab60823e269bd7b432ac43df13667d1160
elloot <elliot.duchek@gmail.com>
2020-02-01 12:27:42 +0100

    add equals method

--- src/Complex.java
+++ src/Complex.java
@@ -1,22 +1,44 @@
+import java.util.Objects;
+
 /**
  * A program that handles complex numbers
  * 2020-01-31
  * Author: Elliot Duchek
  */
 public class Complex {
-    private double real;
-    private double imag;
+    private double a;
+    private double b;
 
-    public Complex(double a, double b) {
-        this.real = a;
-        this.imag = b;
+    public Complex(double real, double imag) {
+        this.a = real;
+        this.b = imag;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Complex complex = (Complex) o;
+        return Double.compare(complex.a, a) == 0 &&
+                Double.compare(complex.b, b) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(a, b);
     }
 
     public double getReal() {
-        return real;
+        return a;
     }
 
     public double getImag() {
-        return imag;
+        return b;
+    }
+
+    public static void main(String[] args) {
+        Complex x = new Complex(2,3);
+        Complex y = new Complex(2,3);
+
     }
 }
